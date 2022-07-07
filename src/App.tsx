a8d5d24bde38a969d823cd87208c5bd5afbc7f2c
jalvaradoWD <josealvarado1337@gmail.com>
2022-07-07 13:33:48 -0500

    First init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <p>Hi, I'm Jose Alvarado</p>
+        <p>I'm a full-stack web developer.</p>
       </header>
+
+      <main>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Portfolio</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </main>
+
+      <footer>
+        <button>Contact</button>
+      </footer>
     </div>
   );
 }
